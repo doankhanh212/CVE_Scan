13094699ff4fb4200851be720b619c9eedd64ade
doankhanh212 <doanhoangquockhanh@gmail.com>
2025-12-29 12:52:49 +0700

    version 1.0.0 final

--- CVSS_threat_metric/Threat_metric/readme.txt
+++
@@ -1,5 +0,0 @@
-cvss4_runtime/
-├── config.py
-├── kev_db.py
-├── cvss4.py
-└── main.py
--- CVSS_threat_metric/kev_module/config.py
+++
@@ -1,7 +0,0 @@
-KEV_URL = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
-
-DB_PATH = "kev.db"
-CACHE_FILE = "kev_cache.json"
-
-# Cache TTL (seconds) – 6 hours
-CACHE_TTL = 6 * 60 * 60
